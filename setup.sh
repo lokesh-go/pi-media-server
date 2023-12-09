c89a0bf358ded892e026dd6086abdd51ffe4ebb1
Sachin Oraon <sachinoraon55@gmail.com>
2023-12-09 15:22:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+set -e
 
-# install docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+# install required packages
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
 
-# enable docker cmd without root permission
-dockerd-rootless-setuptool.sh install
-
-
+# check if docker is installed already
+if [ `which docker | wc -l` -eq 0 ]
+then
+  echo "Docker is not installed.. Installing Docker"
+  # install docker
+  sudo curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
+  sudo sh /tmp/get-docker.sh
+  echo "Docker installed successfully.. Enabling rootless mode"
+  # enable docker cmd without root permission
+  dockerd-rootless-setuptool.sh install
+else
+  echo "Docker is alread installed at `which docker`"
+fi
